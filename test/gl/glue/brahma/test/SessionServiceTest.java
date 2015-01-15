03a9f7fe726068df5de0b1531746da4ff1aea474
Miguel Sánchez <miguel@glue.gl>
2015-01-15 11:39:30 +0100

    SessionServiceTest.java: Added tests for sessions service

--- test/gl/glue/brahma/test/SessionServiceTest.java
+++ test/gl/glue/brahma/test/SessionServiceTest.java
@@ -1,4 +1,39 @@
 package gl.glue.brahma.test;
 
+import gl.glue.brahma.model.session.Session;
+import gl.glue.brahma.service.SessionService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class SessionServiceTest extends TransactionalTest {
+
+    private SessionService sessionService = new SessionService();
+
+    @Test // Request with invalid user Authentication. User "testClient2" is not an user for session 90700
+    public void returnSessionWithInvalidAuthentication() {
+        int session = 90700;
+        String login = "testClient2";
+        Session result = sessionService.get(session, login);
+        assertEquals(null, result);
+    }
+
+    @Test // Request with an non-existent session id
+    public void returnSessionInvalidId() {
+        int session = 0;
+        String login = "testClient1";
+        Session result = sessionService.get(session, login);
+        assertEquals(null, result);
+    }
+
+    @Test // Valid request
+    public void returnSessionOk() {
+        int session = 90700;
+        String login = "testClient1";
+        Session result = sessionService.get(session, login);
+        assertNotNull(result);
+        assertEquals(session, result.getId());
+    }
+
 }
