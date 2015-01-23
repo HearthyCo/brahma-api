cc03dda8489bcea1812b87e2bcddca07240c7d14
Miguel Sánchez <miguel@glue.gl>
2015-01-23 13:53:27 +0100

    BalanceControllerTest.java, BalanceServiceTest.java: Added tests for balance

---
+++ test/gl/glue/brahma/test/BalanceControllerTest.java
@@ -0,0 +1,48 @@
+package gl.glue.brahma.test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Test;
+import play.mvc.Result;
+import play.test.FakeRequest;
+import utils.TestUtils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static play.test.Helpers.*;
+
+public class BalanceControllerTest extends TransactionalTest {
+
+    @Test // Request balance without user authentication
+    public void requestBalanceWithoutAuthentication() {
+        FakeRequest fr = fakeRequest(GET, "/v1/user/balance");
+        Result result = routeAndCall(fr, REQUEST_TIMEOUT);
+        assertNotNull(result);
+        assertEquals(result.toScala().header().status(), 401);
+    }
+
+    @Test // Request balance success
+    public void requestBalanceOk() {
+        String login = "testClient1";
+        Result responseLogin = TestUtils.makeLoginRequest(login, login);
+
+        Result result = TestUtils.getBalanceRequest(responseLogin);
+
+        assertNotNull(result);
+        assertEquals(result.toScala().header().status(), 200);
+
+        ObjectNode ret = TestUtils.toJson(result);
+
+        JsonNode balance = ret.get("balance");
+        assertEquals(balance.get("amount").asInt(), 20000000);
+
+        assertEquals(balance.get("transactions").size(), 4);
+
+        int sum = 0;
+        for(JsonNode transaction : balance.get("transactions")) {
+            sum += transaction.get("amount").asInt();
+        }
+
+        assertEquals(ret.get("balance").get("amount").asInt(), sum);
+    }
+}
---
+++ test/gl/glue/brahma/test/BalanceServiceTest.java
@@ -0,0 +1,38 @@
+package gl.glue.brahma.test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import gl.glue.brahma.service.BalanceService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class BalanceServiceTest extends TransactionalTest  {
+
+    private BalanceService balanceService = new BalanceService();
+
+    @Test // Request sessions with valid user Authentication
+    public void requestBalanceOk() {
+        int uid = 90000;
+        ObjectNode result = balanceService.getBalance(uid);
+
+        assertNotNull(result);
+        assertEquals(result.get("amount").asInt(), 20000000);
+        assertEquals(result.get("transactions").size(), 4);
+
+        int sum = 0;
+        for(JsonNode transaction : result.get("transactions")) {
+            sum += transaction.get("amount").asInt();
+        }
+
+        assertEquals(result.get("amount").asInt(), sum);
+    }
+
+    @Test // Request sessions with valid user Authentication
+    public void requestAmountOk() {
+        int uid = 90000;
+        int result = balanceService.getAmount(uid);
+        assertEquals(result, 20000000);
+    }
+}
