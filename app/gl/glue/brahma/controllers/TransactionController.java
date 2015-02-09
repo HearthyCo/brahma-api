8a3f2d4e31aa63c730f8001b6c0f75ec1f74a309
Anxo Soto <anxo@glue.gl>
2015-02-09 18:17:14 +0100

    Documentadas APIs de pago de Paypal.

--- app/gl/glue/brahma/controllers/TransactionController.java
+++ app/gl/glue/brahma/controllers/TransactionController.java
@@ -87,6 +87,46 @@ public class TransactionController extends Controller {
         return ok(result);
     }
 
+    /**
+     * @api {post} /transaction CreateTransaction
+     *
+     * @apiGroup Transaction
+     * @apiName CreatePaypalTransaction
+     * @apiDescription Create a new Paypal transaction and return info about it.
+     *
+     * @apiParam {int}    amount    The amount of money to spend, in cents.
+     * @apiParamExample {json} Request-Example
+     *      {
+     *          "amount": "1000"
+     *      }
+     *
+     * @apiSuccess {object}      transaction    The newly created Paypal transaction
+     * @apiSuccess {string}      redirect       The external URL to complete the payment
+     * @apiSuccessExample {json} Success-Response
+     *      HTTP/1.1 200 OK
+     *      {
+     *          "transaction": {
+     *              "id": 5,
+     *              "amount": 1000,
+     *              "state": "INPROGRESS",
+     *              "sku": "PAY-6KM12958S00676842KTMOFLI",
+     *              "timestamp": 1423499949564,
+     *              "reason": "Topup your account",
+     *              "meta": {}
+     *          },
+     *          "redirect": "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=EC-8DJ03018CJ419243D"
+     *      }
+     *
+     * @apiError {Object} UserNotLoggedIn User is not logged in.
+     * @apiErrorExample {json} UserNotLoggedIn
+     *      HTTP/1.1 401 Unauthorized
+     *      {
+     *          "status": "401",
+     *          "title": "You are not logged in"
+     *      }
+     *
+     * @apiVersion 0.1.0
+     */
     @BasicAuth
     @Transactional
     @BodyParser.Of(BodyParser.Json.class)
@@ -111,13 +151,70 @@ public class TransactionController extends Controller {
         Transaction transaction;
         transaction = transactionService.createPaypalTransaction(uid, amount, baseUrl);
 
-        // Get session with login
+        // Put the transaction in the response
         result = Json.newObject();
         result.put("transaction", Json.toJson(transaction));
+
+        // Copy the paypal approval URL to the redirect field
+        String redir = transaction.getMeta().get("paypal").get("links").get(1).get("href").asText();
+        result.put("redirect", redir);
 
         return ok(result);
     }
 
+
+    /**
+     * @api {post} /transaction/execute ExecuteTransaction
+     *
+     * @apiGroup Transaction
+     * @apiName ExecutePaypalTransaction
+     * @apiDescription Completes a Paypal transaction.
+     *
+     * @apiParam {string}    PayerID      The PayerID returned by Paypal after approving the transaction
+     * @apiParam {string}    paymentId    The paymentId returned by Paypal after approving the transaction
+     * @apiParam {string}    token        The token returned by Paypal after approving the transaction
+     * @apiParamExample {json} Request-Example
+     *      {
+     *          "PayerID": "M8PEV8LHANHJY"
+     *          "paymentId": "PAY-98B38881S9937240WKTMOI6Q"
+     *          "token": "EC-0HH25056CY103791W"
+     *      }
+     *
+     * @apiSuccess {object}      transaction    The completed Paypal transaction
+     * @apiSuccess {int}         balance        The updated user balance after the transaction
+     * @apiSuccessExample {json} Success-Response
+     *      HTTP/1.1 200 OK
+     *      {
+     *          "transaction": {
+     *              "id": 5,
+     *              "amount": 1000,
+     *              "state": "APPROVED",
+     *              "sku": "PAY-6KM12958S00676842KTMOFLI",
+     *              "timestamp": 1423499949564,
+     *              "reason": "Topup your account",
+     *              "meta": {}
+     *          },
+     *          "balance": 1000
+     *      }
+     *
+     * @apiError {Object} UserNotLoggedIn User is not logged in.
+     * @apiErrorExample {json} UserNotLoggedIn
+     *      HTTP/1.1 401 Unauthorized
+     *      {
+     *          "status": "401",
+     *          "title": "You are not logged in"
+     *      }
+     *
+     * @apiError {Object} TransactionInvalid Transaction cannot be executed.
+     * @apiErrorExample {json} TransactionInvalid
+     *      HTTP/1.1 412 Precondition failed
+     *      {
+     *          "status": "412",
+     *          "title": "Transaction is not executable"
+     *      }
+     *
+     * @apiVersion 0.1.0
+     */
     @BasicAuth
     @Transactional
     @BodyParser.Of(BodyParser.Json.class)
