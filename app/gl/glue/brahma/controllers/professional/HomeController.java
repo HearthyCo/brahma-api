7d19c155c109756c9d9880c226b8b66644e1fbc9
Miguel <miguel@glue.gl>
2015-02-23 18:10:07 +0100

    HomeController.java: Added HomeController HomeControllerTest.java: Added test for HomeController

--- app/gl/glue/brahma/controllers/professional/HomeController.java
+++ app/gl/glue/brahma/controllers/professional/HomeController.java
@@ -1,8 +1,34 @@
 package gl.glue.brahma.controllers.professional;
 
+import actions.ProfessionalAuth;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import gl.glue.brahma.model.service.Service;
+import gl.glue.brahma.model.servicetype.ServiceType;
+import gl.glue.brahma.model.session.Session;
+import gl.glue.brahma.model.sessionuser.SessionUser;
+import gl.glue.brahma.model.transaction.Transaction;
+import gl.glue.brahma.model.user.User;
+import gl.glue.brahma.service.ServiceService;
+import gl.glue.brahma.service.SessionService;
+import gl.glue.brahma.service.UserService;
+import play.Logger;
+import play.db.jpa.Transactional;
+import play.libs.Json;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class HomeController extends Controller {
+
+    private static SessionService sessionService = new SessionService();
+    private static ServiceService serviceService = new ServiceService();
 
     /**
      * @api {get} /user/home Homepage
@@ -38,16 +64,53 @@ public class HomeController extends Controller {
      *
      * @apiVersion 0.1.0
      */
-    /*
     @ProfessionalAuth
     @Transactional
     @BodyParser.Of(BodyParser.Json.class)
     public static Result getHome() {
+
         int uid = Integer.parseInt(session("id"));
 
-        ObjectNode result = Json.newObject();
+        // Create State Session List Array for iterate and pass DAO function a Session.State ArrayList
+        List<Service> services = serviceService.getServicesOfUser(uid);
+        Set<Session.State> states = EnumSet.of(Session.State.UNDERWAY, Session.State.CLOSED);
 
-        return ok(result);
+        ArrayNode sessions = new ArrayNode(JsonNodeFactory.instance);
+        ArrayNode serviceTypes = new ArrayNode(JsonNodeFactory.instance);
+        ArrayNode sessionsByServiceType = new ArrayNode(JsonNodeFactory.instance);
+
+        Map<Integer, Integer> poolsSize = sessionService.getPoolsSize();
+        Logger.info("POOL " + poolsSize);
+        // Iterate State Session List Array
+        for (Service service : services) {
+            ServiceType serviceType = service.getServiceType();
+            serviceTypes.add(Json.toJson(serviceType));
+
+            int serviceTypeId = serviceType.getId();
+            List<SessionUser> sessionUsers = sessionService.getUserSessionsByService(uid, serviceTypeId, states);
+
+            ObjectNode thisService = Json.newObject();
+            ArrayNode sessionsThisService = new ArrayNode(JsonNodeFactory.instance);
+            for(SessionUser sessionUser : sessionUsers) {
+                Session session = sessionUser.getSession();
+
+                sessions.add(Json.toJson(session));
+                sessionsThisService.add(session.getId());
+
+            }
+
+            int queue = poolsSize.containsKey(serviceTypeId) ? poolsSize.get(serviceTypeId) : 0;
+            thisService.put("serviceType", serviceType.getId());
+            thisService.put("waiting", queue);
+            thisService.put("sessions", sessionsThisService);
+
+            sessionsByServiceType.add(thisService);
+        }
+
+        return ok(Json.newObject()
+                .putPOJO("home", Json.newObject()
+                        .putPOJO("services", sessionsByServiceType))
+                .putPOJO("sessions", sessions)
+                .putPOJO("serviceTypes", serviceTypes));
     }
-    */
 }
---
+++ test/gl/glue/brahma/test/controllers/professional/HomeControllerTest.java
@@ -0,0 +1,48 @@
+package gl.glue.brahma.test.controllers.professional;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Test;
+import play.Logger;
+import play.mvc.Result;
+import play.test.FakeRequest;
+import utils.TestUtils;
+import utils.TransactionalTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static play.mvc.Http.Status.REQUEST_TIMEOUT;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.fakeRequest;
+import static play.test.Helpers.routeAndCall;
+
+public class HomeControllerTest extends TransactionalTest {
+
+    @Test // Request without user authentication
+    public void testGetHomeUnauthenticated() {
+        FakeRequest fr = fakeRequest(GET, "/v1/professional/me/home");
+        Result result = routeAndCall(fr, REQUEST_TIMEOUT);
+        assertNotNull(result);
+        assertEquals(401, result.toScala().header().status());
+    }
+
+    @Test // Request home
+    public void testGetHomeOk() {
+        String login = "testProfessional1@glue.gl";
+
+        Result auth = TestUtils.makeProfessionalLoginRequest(login, login);
+        Result result = TestUtils.callController(GET, "/v1/professional/me/home", auth);
+
+        assertNotNull(result);
+        assertEquals(200, result.toScala().header().status());
+
+        ObjectNode ret = TestUtils.toJson(result);
+
+        // Sessions
+        JsonNode services = ret.get("home").get("services");
+        JsonNode sessions = ret.get("sessions");
+
+        assertEquals(3, services.size());
+        assertEquals(0, sessions.size());
+    }
+}
