95a64a9055b7c5acc5fc01bc065065dbee90ff7e
Anxo Soto <anxo@glue.gl>
2015-03-12 11:11:20 +0100

    Creados endpoints para confirmar correo, solicitar nueva contraseña, y completar cambio de contraseña.

--- app/gl/glue/brahma/controllers/common/UserController.java
+++ app/gl/glue/brahma/controllers/common/UserController.java
@@ -1,11 +1,20 @@
 package gl.glue.brahma.controllers.common;
 
+import actions.BasicAuth;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import gl.glue.brahma.model.user.User;
+import gl.glue.brahma.service.UserService;
+import gl.glue.brahma.util.JsonUtils;
 import play.db.jpa.Transactional;
 import play.libs.Json;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class UserController extends Controller {
+
+    private static UserService userService = new UserService();
 
     /**
      * @api {post} /user/logout Logout
@@ -25,6 +34,55 @@ public class UserController extends Controller {
     public static Result logout() {
         session().clear();
         return ok(Json.newObject());
+    }
+
+    @BasicAuth
+    @Transactional
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result confirmMail() {
+        JsonNode json = request().body().asJson();
+        ObjectNode result = JsonUtils.checkRequiredFields(json, "hash");
+        if (result != null) return badRequest(result);
+
+        int uid = Integer.parseInt(session("id"));
+        if (userService.confirmMail(uid, json.get("hash").asText())) {
+            return ok(Json.newObject());
+        } else {
+            return status(403, JsonUtils.simpleError("403", "Hash validation failed."));
+        }
+    }
+
+    @Transactional
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result requestPasswordChange() {
+        JsonNode json = request().body().asJson();
+        ObjectNode result = JsonUtils.checkRequiredFields(json, "email");
+        if (result != null) return badRequest(result);
+
+        User user = userService.requestPasswordChange(json.get("email").asText());
+        if (user != null) {
+            return ok(Json.newObject());
+        } else {
+            return status(404, JsonUtils.simpleError("404", "User not found."));
+        }
+    }
+
+    @Transactional
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result confirmPasswordChange() {
+        JsonNode json = request().body().asJson();
+        ObjectNode result = JsonUtils.checkRequiredFields(json, "email", "hash", "newPassword");
+        if (result != null) return badRequest(result);
+
+        String email = json.get("email").asText();
+        String hash = json.get("hash").asText();
+        String newPassword = json.get("newPassword").asText();
+        if (userService.confirmPasswordChange(email, hash, newPassword)) {
+            // Should this also perform a login?
+            return ok(Json.newObject());
+        } else {
+            return status(403, JsonUtils.simpleError("403", "Hash validation failed."));
+        }
     }
 
 }
--- app/gl/glue/brahma/service/UserService.java
+++ app/gl/glue/brahma/service/UserService.java
@@ -1,5 +1,7 @@
 package gl.glue.brahma.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import gl.glue.brahma.model.user.User;
 import gl.glue.brahma.model.user.UserDao;
 import gl.glue.brahma.util.Mailer;
@@ -40,11 +42,47 @@ public class UserService {
 
     @Transactional
     public User getById(int uid) {
-        User user = userDao.findById(uid);
-        if (user != null) {
-            return user;
-        } else {
-            return null;
-        }
+        return userDao.findById(uid);
     }
+
+    @Transactional
+    public boolean confirmMail(int uid, String hash) {
+        User user = userDao.findById(uid);
+        JsonNode mailConfirm = user.getMeta().path("confirm").path("mail");
+        if (user.isConfirmed() || mailConfirm.isMissingNode()) return false;
+        if (System.currentTimeMillis() > mailConfirm.get("expires").asLong()) return false;
+        if (!mailConfirm.get("hash").asText().equals(hash)) return false;
+        user.setConfirmed(true);
+        // If adding another welcome mail, send it from here, like this:
+        // Mailer.send(user, REGISTER_COMPLETE_MAIL);
+        return true;
+    }
+
+    @Transactional
+    public User requestPasswordChange(String email) {
+        User user = userDao.findByEmail(email);
+        if (user == null) return null;
+        user.mergeMeta(Json.newObject()
+                .putPOJO("confirm", Json.newObject()
+                        .putPOJO("password", Json.newObject()
+                                .put("hash", RandomStringUtils.randomAlphanumeric(32))
+                                .put("expires", System.currentTimeMillis() + 86400000))));
+        Mailer.send(user, Mailer.MailTemplate.RECOVER_CONFIRM_MAIL);
+        return user;
+    }
+
+    @Transactional
+    public boolean confirmPasswordChange(String email, String hash, String newPassword) {
+        User user = userDao.findByEmail(email);
+        JsonNode meta = user.getMeta();
+        JsonNode passConfirm = meta.path("confirm").path("password");
+        if (passConfirm.isMissingNode()) return false;
+        if (System.currentTimeMillis() > passConfirm.get("expires").asLong()) return false;
+        if (!passConfirm.get("hash").asText().equals(hash)) return false;
+        ((ObjectNode)meta.get("confirm")).remove("password");
+        user.setMeta(meta);
+        user.setPassword(newPassword);
+        return true;
+    }
+
 }
