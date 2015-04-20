7c1d2c1b0ad99bf2adc185b4d75861d82ec325a3
Anxo Soto <anxo@glue.gl>
2015-04-20 18:43:40 +0200

    Moved Vagrant from API to misc.

--- vagrant/docker-entrypoint-initdb.d/create-test-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "*** Creating test database..."
-
-gosu postgres postgres --single -E <<-EOSQL
-    CREATE DATABASE "brahmatest"
-EOSQL
--- vagrant/ssh_docker.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-vagrant ssh $(vagrant global-status | grep 'default' | awk '{print $1}')
